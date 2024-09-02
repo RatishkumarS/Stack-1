f0f64310b3806bfdb76ff01cca13d67668467262
Ratish Kumar Saravanan <ratishkumarsaravanan@Ratishs-Air-4.lan>
2024-09-01 23:25:21 -0400

    Stack-1 done

--- sample.java
+++ sample.java
@@ -5,3 +5,42 @@
 
 
 // Your code here along with comments explaining your approach
+
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack=new Stack<>();
+        int[] res=new int[temperatures.length];
+        int n=temperatures.length;
+        for(int i=0;i<n;i++){
+            while(!stack.isEmpty() && temperatures[i]>temperatures[stack.peek()]){
+                int poped=stack.pop();
+                res[poped] = i - poped;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}
+
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        Stack<Integer> stack= new Stack<>();
+        int n=nums.length;
+        int[] res=new int[n];
+        for(int i=0;i<n;i++)
+            res[i]=-1;
+        for(int i=0;i<2*n;i++)
+        {
+            int index=i%n;
+            while(!stack.isEmpty() && nums[index]>nums[stack.peek()]){
+                int poped=stack.pop();
+                res[poped]=nums[index];
+            }
+            if(i<n)
+                stack.push(index);
+        }
+        return res;
+    }
+}
